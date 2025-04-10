c95b0372b9ebea6479eece59991e450077673672
Mengyao Liu <87812974+Lemenyeux@users.noreply.github.com>
2025-04-10 13:35:10 +0800

    Update Install_packages.R

--- Scripts/Install_packages.R
+++ Scripts/Install_packages.R
@@ -1,12 +1,11 @@
 ## pakages use in our scripts--------------------------
 pkgs <- c("data.table", "dplyr", "ggplot2", "corrplot", "ggpubr", "Gmisc", "openxlsx", "readxl", "paletteer", "MuMIn",
-          "ggsci", "scales", "RColorBrewer", "gridExtra", "dplyr", "tidyr", "stringr", "colorspace", "cowplot",
+          "ggsci", "scales", "RColorBrewer", "gridExtra", "tidyr", "stringr", "colorspace", "cowplot",
           "ggbreak", "rstatix", "randomForest", "forestploter", "sem", "lavaan", "bruceR", "tidyverse", "patchwork",
           "ggridges", "dorothea", "OmnipathR", "decoupleR", "viridis", "ggpattern", "biomaRt", "dbplyr", 
-          "GenomicRanges", "LOLA", "simpleCache", "ggbeeswarm", "RColorBrewer", 
+          "GenomicRanges", "LOLA", "simpleCache", "ggbeeswarm",  
           "genekitr", "igraph", "ggraph", "rrvgo", "clusterProfiler", "enrichplot", 
-          "janitor", "binom", "glue", "lawstat", "weights", "epitools", "DescTools", "openxlsx", "optparse", "MASS",
-          "colorspace", "cowplot")
+          "janitor", "binom", "glue", "lawstat", "weights", "epitools", "DescTools", "optparse", "MASS")
 
 
 miss_pkgs <- setdiff(pkgs, installed.packages()[, "Package"])
@@ -15,22 +14,22 @@ bio_pkgs <- c("biomaRt", "GenomicRanges", "LOLA", "clusterProfiler", "enrichplot
 
 ## install the missing packages--------------------------
 for(i in miss_pkgs){
-    
+  
   tryCatch({
-      if(i %in% bio_pkgs) {
-        
-        # If it's a Bioconductor package, install using BiocManager
-        if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
-        BiocManager::install(i)
-      } else {
-        
-        # If it's a CRAN package, install using install.packages
-        install.packages(i)
-      }
-    }, error = function(e) {
+    if(i %in% bio_pkgs) {
       
-      cat("Error installing package:", i, "\n")
-      message("Error message:", e$message, "\n")
-    })
-
-  }
+      # If it's a Bioconductor package, install using BiocManager
+      if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
+      BiocManager::install(i)
+    } else {
+      
+      # If it's a CRAN package, install using install.packages
+      install.packages(i)
+    }
+  }, error = function(e) {
+    
+    cat("Error installing package:", i, "\n")
+    message("Error message:", e$message, "\n")
+  })
+  
+}
