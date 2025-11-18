2a27cb49b841a8e0c655683b84f5bbdac4f42365
Mengyao Liu <87812974+Lemenyeux@users.noreply.github.com>
2025-11-18 14:15:27 +0800

    Add files via upload

---
+++ Scripts/1.Install_packages.R
@@ -0,0 +1,35 @@
+## pakages use in our scripts--------------------------
+pkgs <- c("data.table", "dplyr", "ggplot2", "corrplot", "ggpubr", "Gmisc", "openxlsx", "readxl", "paletteer", "MuMIn",
+          "ggsci", "scales", "RColorBrewer", "gridExtra", "tidyr", "stringr", "colorspace", "cowplot",
+          "ggbreak", "rstatix", "randomForest", "forestploter", "sem", "lavaan", "bruceR", "tidyverse", "patchwork",
+          "ggridges", "dorothea", "OmnipathR", "decoupleR", "viridis", "ggpattern", "biomaRt", "dbplyr", 
+          "GenomicRanges", "LOLA", "simpleCache", "ggbeeswarm",  
+          "genekitr", "igraph", "ggraph", "rrvgo", "clusterProfiler", "enrichplot", 
+          "janitor", "binom", "glue", "lawstat", "weights", "epitools", "DescTools", "optparse", "MASS","mediation", "org.Hs.eg.db")
+
+
+miss_pkgs <- setdiff(pkgs, installed.packages()[, "Package"])
+bio_pkgs <- c("biomaRt", "GenomicRanges", "LOLA", "clusterProfiler", "enrichplot", 
+              "dorothea", "OmnipathR", "decoupleR", "org.Hs.eg.db")
+
+## install the missing packages--------------------------
+for(i in miss_pkgs){
+  
+  tryCatch({
+    if(i %in% bio_pkgs) {
+      
+      # If it's a Bioconductor package, install using BiocManager
+      if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
+      BiocManager::install(i)
+    } else {
+      
+      # If it's a CRAN package, install using install.packages
+      install.packages(i)
+    }
+  }, error = function(e) {
+    
+    cat("Error installing package:", i, "\n")
+    message("Error message:", e$message, "\n")
+  })
+  
+}
