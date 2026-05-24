729c23e204c84c6e5a4c46dc0bab2c375d4dfb4d
Mengyao Liu <87812974+Lemenyeux@users.noreply.github.com>
2026-05-24 20:48:52 +0800

    Update and rename Other.R to 10. Other.R

--- Scripts/10. Other.R
+++ Scripts/10. Other.R
@@ -101,7 +101,7 @@ fisher_test_list_pn
 
 ####################PCA analysis#####################--------------------
 #1 load data-----------------
-GPS_feature_all <- fread("GPS_feature_all.csv")
+GPS_feature_all <- fread("GPS_feature_all.csv", na.strings = c("", "NA"))
 head(GPS_feature_all, 2)
 
 #"ogee_connectivity" #6467 NAs
